6e6323a12fd3617b0ac86f690c1e60cb216ee350
Rekash <benmehal.joris@gmail.com>
2024-05-06 14:29:51 +0200

    💄  Add new projects to the homepage

--- src/assets/containers/HomePage/Projects/Projects.jsx
+++ src/assets/containers/HomePage/Projects/Projects.jsx
@@ -3,6 +3,8 @@ import "./projects.scss";
 import ChezValerie from "../../../images/chezvalerie-videgrenier.fr.png";
 import BJTreklife from "../../../images/bj-treklife.fr.png";
 import ArgentBank from "../../../images/ArgentBank.png";
+import ArticleSheet from "../../../images/chezvalerie-article-sheet.png";
+import Portfolio from "../../../images/portfolio.png";
 import Slider from "react-slick";
 import { useTranslation } from "react-i18next";
 
@@ -26,12 +28,36 @@ function Projects() {
       title: "BJ-Treklife",
       description: t("Projects.BJTreklife"),
       image: BJTreklife,
+      github: "https://github.com/notREKASH/bj-treklife",
       website: "https://www.bj-treklife.fr/",
       filters: [
         { name: "NextJS", bgColor: "#252F47", fontColor: "#FFF" },
         { name: "SASS", bgColor: "#392647", fontColor: "#f784f5" },
         { name: "Express", bgColor: "#7C7707", fontColor: "#FFFF00" },
         { name: "MongoDb", bgColor: "#2a4725", fontColor: "#84f786" },
+      ],
+    },
+    {
+      title: "Portfolio",
+      description: t("Projects.Portfolio"),
+      image: Portfolio,
+      github: "https://github.com/notREKASH/Portfolio",
+      filters: [
+        { name: "React", bgColor: "#252F47", fontColor: "#82dff6" },
+        { name: "SASS", bgColor: "#392647", fontColor: "#f784f5" },
+        { name: "Express", bgColor: "#7C7707", fontColor: "#FFFF00" },
+        { name: "AG Grid", bgColor: "#0e4491", fontColor: "#5c9cf7" },
+      ],
+    },
+    {
+      title: t("Projects.ArticleSheets.Title"),
+      description: t("Projects.ArticleSheets.Description"),
+      image: ArticleSheet,
+      github: "https://github.com/notREKASH/Form-XLSX",
+      filters: [
+        { name: "React", bgColor: "#252F47", fontColor: "#82dff6" },
+        { name: "SASS", bgColor: "#392647", fontColor: "#f784f5" },
+        { name: "XLSX", bgColor: "#252F47", fontColor: "#82dff6" },
       ],
     },
     {
